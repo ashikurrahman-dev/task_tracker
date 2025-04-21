696ded330801cb7b66c67a2eda594a19579e3075
ashik <ashikurrahman7134@gmail.com>
2025-04-22 00:05:21 +0600

    reset password done

--- controller/auth/forgetPasswordController.php
+++ controller/auth/forgetPasswordController.php
@@ -88,3 +88,34 @@ function verifyOTP()
         }
     }
 }
+
+
+function resetPassword(){
+    global $pdo;
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        try{
+            $email = validateInput($_REQUEST['email']);
+            $password = validateInput($_POST['password']);
+            $retype_password = validateInput($_POST['retype_password']);
+            
+            if($password === '' || $retype_password === ''){
+                throw new Exception('Password field can not be empty.');
+            }
+            if(strlen($password) < 6){
+                throw new Exception('Password minimun 6 charecters');
+            }
+            if($password !== $retype_password){
+                throw new Exception('Password must be match');
+            }
+    
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $stmt = $pdo->prepare("UPDATE users SET password=? WHERE email=?");
+            $stmt->execute([$password, $email]);
+    
+            header("Location: " . BASE_URL . "views/auth/login.php");
+            exit();
+        } catch(Exception $e){
+            return $e->getMessage();
+        }
+    }
+}
---
+++ views/auth/resetPassword.php
@@ -0,0 +1,47 @@
+<?php
+require_once __DIR__ ."/../../controller/auth/forgetPasswordController.php";
+$error = resetPassword();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Register Page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded-2xl shadow-2xl w-full max-w-md">
+        <h2 class="text-3xl font-bold text-center text-gray-800 mb-6">Reset Password</h2>
+        <form action="" method="POST" class="space-y-5">
+            
+            <div>
+                <label class="block mb-1 text-gray-600">New Password</label>
+                <input type="password" name="password"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500" />
+            </div>
+            <div>
+                <label class="block mb-1 text-gray-600">Confirm Password</label>
+                <input type="password" name="retype_password"
+                    class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500" />
+            </div>
+            <?php if (isset($error)): ?>
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+                    <?= $error ?>
+                </div>
+            <?php endif; ?>
+            <button type="submit"
+                class="w-full bg-purple-600 text-white py-2 rounded-lg hover:bg-purple-700 transition duration-300">
+                Submit
+            </button>
+        </form>
+        <div class="flex items-center mt-4 text-sm">
+            <a href="<?= BASE_URL; ?>public/login.php" class="text-purple-600 hover:underline">Back
+                to Sign In</a>
+        </div>
+    </div>
+</body>
+</html>
