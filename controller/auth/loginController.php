d818abe56b486ab0dae3bbfac072101676298477
ashik <ashikurrahman7134@gmail.com>
2025-04-24 21:26:41 +0600

    update

--- controller/auth/loginController.php
+++ controller/auth/loginController.php
@@ -1,43 +1,43 @@
-<?php 
+<?php
 require_once __DIR__ . '/../../includes/functions.php';
 
-// if(isset($_SESSION["user"])){
-//     header("Location: " . BASE_URL ."public/dashboard.php");
-//     exit();
-// }
+if (isset($_SESSION['user'])) {
+    header('Location: ' . BASE_URL . 'controller/taskController.php');
+    exit();
+}
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    try{
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
         $email = validateInput($_POST['email']);
         $password = validateInput($_POST['password']);
 
-        if($email === ''){
+        if ($email === '') {
             throw new Exception('Email can not be empty');
         }
-        if($password === ''){
+        if ($password === '') {
             throw new Exception('Password can not be empty');
         }
-        
+
         validatePassword($password);
         validateEmail($email);
 
         $stmt = $pdo->prepare("SELECT * FROM users WHERE email=?");
         $stmt->execute([$email]);
 
-        if($stmt->rowCount() === 0){
+        if ($stmt->rowCount() === 0) {
             throw new Exception("Email is not found");
         }
         $user = $stmt->fetch();
 
-        if(!password_verify($password, $user["password"])){
+        if (!password_verify($password, $user["password"])) {
             throw new Exception("Password does not match.");
         }
 
         $_SESSION['user'] = $user;
-        header('Location:' . BASE_URL . 'views/task/index.php');
+        header('Location:' . BASE_URL . 'controller/taskController.php');
         exit();
 
-    } catch(Exception $e){
+    } catch (Exception $e) {
         $error = $e->getMessage();
-    }    
+    }
 }
